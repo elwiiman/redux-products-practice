af4d734873fb5b8499d1399c4c569c2907c71437
Emmanuel Sanchez <emmanuelsanchez@MacBook-Pro-de-erik-2.local>
2020-06-18 22:55:52 -0500

    actions and reducers for create a product created

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -4,7 +4,29 @@ import { ADD_PRODUCT, ADD_PRODUCT_SUCCESS, ADD_PRODUCT_ERROR } from "../types";
 
 //create new products
 export function createNewProductAction(product) {
-  return () => {
-    console.log(product);
+  return (dispatch) => {
+    dispatch(addProduct());
+    try {
+      dispatch(addProductSuccess(product));
+    } catch (error) {
+      dispatch(addProductError(true));
+    }
   };
 }
+
+const addProduct = () => ({
+  type: ADD_PRODUCT,
+  payload: true,
+});
+
+// if products is saved in database
+const addProductSuccess = (product) => ({
+  type: ADD_PRODUCT_SUCCESS,
+  payload: product,
+});
+
+//if errror
+const addProductError = (errorState) => ({
+  type: ADD_PRODUCT_ERROR,
+  payload: errorState,
+});
